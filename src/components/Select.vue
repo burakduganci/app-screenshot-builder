2cdedfc5cf44bfe93f53c31bb9cc8a79ca57ab41
Burak Duğancı <burakduganci@gmail.com>
2019-11-07 12:22:00 +0300

    text-color and bg color added

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="relative">
         <select class="shadow block appearance-none w-full bg-white border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-            <option>New Mexico</option>
-            <option>Missouri</option>
-            <option>Texas</option>
+            <option>Nexus</option>
+            <option>iPhone</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
